5c60b4143328c236137060d4ec0926b0951c9d5e
will004 <williamalfredo85@gmail.com>
2019-12-06 16:53:31 +0700

    can register, login, and check user session. also can validate access to a page i.e. must be logged in to access that page

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[
@@ -20,6 +21,16 @@ class RegistrationForm(FlaskForm):
     ])
 
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username is already exists')
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email is already exists')
 
 class LoginForm(FlaskForm):    
     email = StringField('Email', validators=[
